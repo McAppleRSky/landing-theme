4417f2fbf8567d7cb57ffb549df095546b02c0a7
usr <usr@debian.dom>
2020-11-01 21:30:55 +0300

    dos init

---
+++ index.php
@@ -0,0 +1,120 @@
+<?php get_header(); ?>
+<main class="main">
+  <div class="container">
+    <div class="main_title">
+      Строительство домов и бань
+      из строганного и профилированного бруса
+    </div>
+    <div class="main_text">
+      За 10 лет мы возвели больше250 домов и бань из бруса
+      ИМЕЕМ БОЛЬШОЙ ОПЫТ В СТРОИТЕЛЬСТВЕ
+      гарантируем качество и надежность всех аостроенных нами объектов
+      ГАРАНТИЯ НА ДОМА И БАНИ ИЗ БРУСА 3 ГОДА
+    </div>
+    <div class="main_img">
+      <img src="<?php bloginfo( 'template_url' ); ?>/assets/img/main_img.png" alt="">
+    </div>
+    <div class="project">
+      <div class="project_title">
+        ПРОЕКТЫ ДОМОВ ИЗ БРУСА
+      </div>
+      <div class="project_item">
+        <div class="project_name">
+          Дом №1
+        </div>
+        <div class="project_title">
+          Размер дома: 6 х 6
+        </div>
+        <div class="project_area">
+          Общая площадь дома: 54 кв. м
+        </div>
+        <div class="project_price">
+          Стоимость - 402 000 рублей
+        </div>
+        <div class="project_images">
+          <div class="project_images-item">
+            <img src="img/project1-1.jpg" alt="">
+          </div>
+          <div class="project_images-item">
+            <img src="img/project1-2.jpg" alt="">
+          </div>
+        </div>
+        <div class="project_name">
+          Дом №1
+        </div>
+        <div class="project_title">
+          Размер дома: 6 х 6
+        </div>
+        <div class="project_area">
+          Общая площадь дома: 54 кв. м
+        </div>
+        <div class="project_price">
+          Стоимость - 402 000 рублей
+        </div>
+        <div class="project_images">
+          <div class="project_images-item">
+            <img src="img/project2-1.jpg" alt="">
+          </div>
+          <div class="project_images-item">
+            <img src="img/project2-2.jpg" alt="">
+          </div>
+        </div>
+        <div class="project_name">
+          Дом №1
+        </div>
+        <div class="project_title">
+          Размер дома: 6 х 6
+        </div>
+        <div class="project_area">
+          Общая площадь дома: 54 кв. м
+        </div>
+        <div class="project_price">
+          Стоимость - 402 000 рублей
+        </div>
+        <div class="project_images">
+          <div class="project_images-item">
+            <img src="img/project3-1.jpg" alt="">
+          </div>
+          <div class="project_images-item">
+            <img src="img/project3-2.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="download">
+    <img src="<?php bloginfo( 'template_url' ); ?>/assets/img/home-download.png" alt="">
+    <a href="#">Скачать весь каталог домов</a>
+  </div>
+  <div class="container">
+    <div class="gallery"></div>
+    <div class="gallery_title">
+      ФОТОГРАФИИ НАШИХ ДОМОВ
+    </div>
+    <div class="gallery_text">
+      некоторые фотографии построенных нами домов и бань из бруса
+    </div>
+    <div class="gallery_inner">
+      <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
+      <a href="img/item-2.jpg"><img src="img/item-2.jpg" alt=""></a>
+      <a href="img/item-3.jpg"><img src="img/item-3.jpg" alt=""></a>
+      <a href="img/item-4.jpg"><img src="img/item-4.jpg" alt=""></a>
+      <a href="img/item-5.jpg"><img src="img/item-5.jpg" alt=""></a>
+      <a href="img/item-6.jpg"><img src="img/item-6.jpg" alt=""></a>
+      <a href="img/item-7.jpg"><img src="img/item-7.jpg" alt=""></a>
+      <a href="img/item-8.jpg"><img src="img/item-8.jpg" alt=""></a>
+      <a href="img/item-9.jpg"><img src="img/item-9.jpg" alt=""></a>
+      <a href="img/item-10.jpg"><img src="img/item-10.jpg" alt=""></a>
+      <a href="img/item-11.jpg"><img src="img/item-11.jpg" alt=""></a>
+      <a href="img/item-12.jpg"><img src="img/item-12.jpg" alt=""></a>
+      <a href="img/item-13.jpg"><img src="img/item-13.jpg" alt=""></a>
+      <a href="img/item-14.jpg"><img src="img/item-14.jpg" alt=""></a>
+      <a href="img/item-15.jpg"><img src="img/item-15.jpg" alt=""></a>
+      <a href="img/item-16.jpg"><img src="img/item-16.jpg" alt=""></a>
+    </div>
+    <div class="main_img">
+      <img src="<?php bloginfo( 'template_url' ); ?>/assets/img/main_img.png" alt="">
+    </div>
+  </div>
+</main>
+<?php get_footer(); ?>
